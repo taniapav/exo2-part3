ba01f8bd346ecc0aa2bdc148f01c2ca4f1683c7b
Tatjana <pavlovic.tatjana.p@gmail.com>
2019-02-01 08:56:08 +0100

    exo2 part3

---
+++ assets/js/js2.js
@@ -0,0 +1,10 @@
+function show(){
+  //Pour afficher le texte
+  document.getElementById('text').style.display='block';
+}
+function hide(){
+  //pour masquer le texte
+  document.getElementById('text').style.display='none';
+}
+//pour masquer la page principal
+document.getElementById('text').style.display='none';
